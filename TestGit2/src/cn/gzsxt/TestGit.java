ff27a98533501ece87d13e06b360878d055cbee5
surexiao <32189186+surexiao@users.noreply.github.com>
2019-02-19 14:56:15 +0800

    Update TestGit.java
    git上直接修改，模拟另一个同事提交的，第三次提交!

--- TestGit2/src/cn/gzsxt/TestGit.java
+++ TestGit2/src/cn/gzsxt/TestGit.java
@@ -5,5 +5,6 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("TestGit2初始化提交！");
 		System.out.println("hello world --->git!");
+		System.out.println("git上直接修改，模拟另一个同事提交的，第三次提交!");
 	}
 }
